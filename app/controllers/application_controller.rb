58e4d7bf32863b8855565d147845a50a4c9916ca
Marc Mills <mdmills1990@gmail.com>
2018-11-21 00:15:42 +0000

    fixed issues with pluralized form of super_heroes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+set :views, Proc.new { File.join(root, "../views/") }
 
 get '/' do
   erb :super_hero
 end
+
+post '/team' do
+  @team = Team.new(params[:team])
+  params[:team][:super_heroes].each do |data|
+    SuperHero.new(data)
+  end
+  @superheroes = SuperHero.all
+  erb :team
+  end
 
 end
---
+++ app/models/super_hero.rb
@@ -0,0 +1,16 @@
+class SuperHero < Sinatra::Base
+  attr_accessor :name, :power, :bio
+  @@superheroes = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@superheroes << self
+  end
+
+  def self.all
+    @@superheroes
+  end
+
+end
